3a837fcb1149d0ad3e026d8f96334909f8ebae98
manar17 <maneeer2006@gmail.com>
2026-02-23 15:58:59 +0200

    remove comments

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -30,26 +30,19 @@ apiClient.interceptors.response.use(
     if (error.response) {
       const { status, data } = error.response;
 
-      // Attempt to use a custom error message from the server
       if (data && data.message) {
         message = data.message;
       } else {
-        // Fallback to predefined error messages
         message = ERROR_MESSAGES[status] || ERROR_MESSAGES.GENERIC_ERROR;
       }
-
-      // Handle 401 Unauthorized
       if (status === 401) {
-        Router.push('/login'); // Redirect to the login page
-      }
     } else {
-      // Handle network errors
       message = ERROR_MESSAGES.NETWORK_ERROR;
     }
 
-    toast.error(message); // Display the error message
-    return Promise.reject(new Error(message)); // Reject the promise with the error message
-  }
+    toast.error(message); 
+    return Promise.reject(new Error(message));
+  }}
 );
 
 
