076ba5ea2f7009d0289215bd2880ed2f463ee5f5
manar17 <maneeer2006@gmail.com>
2026-04-18 16:04:50 +0300

    using nginx as a revers proxy for the frontend

---
+++ backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+composer install --no-interaction --optimize-autoloader
+
+php artisan migrate --force
+php artisan db:seed --force        
+
+echo "Fixing storage permissions"
+chown -R www-data:www-data /var/www/html/storage
+chown -R www-data:www-data /var/www/html/bootstrap/cache
+
+php-fpm
