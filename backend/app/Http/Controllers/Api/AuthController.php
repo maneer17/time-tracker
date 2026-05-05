d50742525b527c6c1dc3438a832e06e689473b03
manar17 <maneeer2006@gmail.com>
2026-05-05 11:26:54 +0300

    add login with google

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Validation\ValidationException;
-use App\Http\Requests\StoreUserRequest; 
+use App\Http\Requests\{StoreUserRequest, GoogleRequest}; 
 use Illuminate\Support\Facades\Hash;
+use Laravel\Socialite\Socialite;
+
 
 class AuthController extends Controller
 {
@@ -53,6 +55,49 @@ class AuthController extends Controller
         $user = $request->user();
         $user->tokens()->delete();
         return response()->json(["message" => "Logged out successfully " . $user->name]);
+    }
+
+
+
+
+
+    public function google(GoogleRequest $request)
+    {
+        $token = $request->validated()['token'];
+
+        try {
+            // userFromToken() works with access tokens 
+            // take access token from frontend and verify it 
+            $googleUser = Socialite::driver('google')
+                ->stateless()
+                ->userFromToken($token);
+
+        } catch (\Exception $e) {
+        return response()->json([
+            'message' => 'Invalid Google token',
+        ], 401);
+    }
+
+        $user = User::firstOrCreate(
+            ['email' => $googleUser->getEmail()],
+            [
+                'name'              => $googleUser->getName(),
+                'google_id'         => $googleUser->getId(),
+                'password'          => null,
+                'email_verified_at' => now(),
+            ]
+        );
+
+        if (!$user->google_id) {
+            $user->update([
+                'google_id' => $googleUser->getId(),
+            ]);
+            // google_id here in case the user changed their email but not their entire account 
+        }
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token, 'user' => $user]);
     }
 
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,36 +13,44 @@ use App\Http\Controllers\Api\{
     MeController,
     ReportController
 };
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::middleware('guest')->controller(AuthController::class)->group(function () {
+
+Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'store');
     Route::post('/login', 'login');
+    Route::post('/auth/google', 'google');
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::apiResource('users', UserController::class)->only(['index']);
+    Route::apiResource('users', UserController::class)
+        ->only(['index']);
 
     Route::apiResource('channels', ChannelController::class);
 
     Route::apiResource('time-entries', TimeEntryController::class);
 
-    Route::apiResource('channels.invitations', InvitationController::class)->scoped();
+    Route::apiResource('channels.invitations', InvitationController::class)
+        ->scoped();
 
     Route::post('shared-days', [SharedDayController::class, 'store']);
-    Route::apiResource('channels.shared-days', SharedDayController::class)
-        ->only(['index', 'destroy', 'show']);
-    Route::apiResource('shared-days.comments', CommentController::class)
-    ->only(['index', 'update', 'destroy', 'store']);
 
-    Route::get('reports',[ReportController::class,'index']);
+    Route::apiResource('channels.shared-days', SharedDayController::class)
+        ->only(['index', 'show', 'destroy']);
+
+    Route::apiResource('shared-days.comments', CommentController::class)
+        ->only(['index', 'store', 'update', 'destroy']);
+
+    Route::get('reports', [ReportController::class, 'index']);
+
     Route::prefix('me')->group(function () {
-        Route::get('shared-days',  [MeController::class, 'sharedDays']);
-        Route::get('invitations',  [MeController::class, 'invitations']);
-        Route::get('channels',     [MeController::class, 'channels']);
-        Route::get('memberships',  [MeController::class, 'memberships']);
+        Route::get('shared-days', [MeController::class, 'sharedDays']);
+        Route::get('invitations', [MeController::class, 'invitations']);
+        Route::get('channels', [MeController::class, 'channels']);
+        Route::get('memberships', [MeController::class, 'memberships']);
     });
 });
